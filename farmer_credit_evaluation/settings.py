edd9544fbd221a2f781257b3844f1d70401f9447
Nishit Patel <nishit.p1@ahduni.edu.in>
2025-03-23 15:12:51 +0530

    Update settings.py

--- farmer_credit_evaluation/settings.py
+++ farmer_credit_evaluation/settings.py
@@ -22,10 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zsh*z+&ykf26!+)t1a1=(8n1#%jpx=ry$gdqa59_ggb0)wu%sv'
+# SECRET_KEY = 'django-insecure-zsh*z+&ykf26!+)t1a1=(8n1#%jpx=ry$gdqa59_ggb0)wu%sv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG=False
+SECRET_KEY='django-insecure-zsh*z+&ykf26!+)t1a1=(8n1#%jpx=ry$gdqa59_ggb0)wu%sv'
+
+
 
 ALLOWED_HOSTS = []
 
