453689ef5aa09e755ad219442893aecabdf9edb9
Nishit Patel <nishit.p1@ahduni.edu.in>
2025-03-23 12:56:10 +0530

    Update settings.py

--- farmer_credit_evaluation/settings.py
+++ farmer_credit_evaluation/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'farmer_credit_evaluation.urls'
